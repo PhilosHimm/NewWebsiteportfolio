4de9d01119d9932c2ff5867eeae05562ff5f66ab
PhilosHimm <161180159+PhilosHimm@users.noreply.github.com>
2025-11-24 17:29:54 -0500

    Codebase refactor
    remove redundant or uncalled components

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -68,20 +68,6 @@ const config = {
         sm: "calc(var(--radius) - 0.4rem)",
         DEFAULT: "var(--radius)",
       },
-      keyframes: {
-        "accordion-down": {
-          from: { height: "0" },
-          to: { height: "var(--radix-accordion-content-height)" },
-        },
-        "accordion-up": {
-          from: { height: "var(--radix-accordion-content-height)" },
-          to: { height: "0" },
-        },
-      },
-      animation: {
-        "accordion-down": "accordion-down 0.2s ease-out",
-        "accordion-up": "accordion-up 0.2s ease-out",
-      },
     },
   },
   plugins: [require("tailwindcss-animate"), require("@tailwindcss/typography")],
